7910aee24d8ba95f11a6317b87163c88ece43309
LeeMasamune <LeeMasamune@gmail.com>
2018-05-03 20:36:18 +0800

    18123
    Changed behavior to use cosine function

--- ledcontrol.py
+++ ledcontrol.py
@@ -1,78 +1,97 @@
 #!/usr/bin/env python3
 # -*- coding: utf_8 -*-
 
-import math
 import random
 import time
+
+import math
+tau = math.pi * 2.0
+
 import pigpio
-
-def Q1(x):
-	if (x < 0):
-		x = 0
-	if (x > 1):
-		x = 1
-	return math.sqrt(1 - (x ** 2))
-
-def Q2(x):
-	if (x < 0):
-		x = 0
-	if (x > 1):
-		x = 1
-	return math.sqrt((2 * x) - (x ** 2))
-
-def Q3(x):
-	if (x < 0):
-		x = 0
-	if (x > 1):
-		x = 1
-	return 1 - math.sqrt((2 * x) - (x ** 2))
-
-def Q4(x):
-	if (x < 0):
-		x = 0
-	if (x > 1):
-		x = 1
-	return 1 - math.sqrt(1 - (x ** 2))
-
 pi = pigpio.pi()
 
-pwm_min = 64
-pwm_max = 255
-pwm_mid = (pwm_min + pwm_max) / 2
-pwm_val = 0
+def QC(x):
+	if (x < 0):
+		x = 0
+	if (x > 1):
+		x = 1
+	d = x * tau
+	z = math.cos(d)
+	return (1 - z) * 0.5
 
-msecs_timeout = 1000 #* random.random()
+def QCR(x):
+	if (x < 0):
+		x = 0
+	if (x > 1):
+		x = 1
+	x = x * 0.5
+	d = x * tau
+	z = math.cos(d)
+	return (1 - z) * 0.5
 
-state = 4
-state_table = { 4 : 2, 2 : 1, 1 : 3, 3 : 4 }
+# unused
+def QCF(x):
+	if (x < 0):
+		x = 0
+	if (x > 1):
+		x = 1
+	x = (x * 0.5) + 0.5
+	d = x * tau
+	z = math.cos(d)
+	return (1 - z) * 0.5	
 
-plot_x = 0
-plot_y = 0
-
-while True:
+def LPR(min, max, msecs_timeout_q):
 	start = time.time()
-	# random from 500 to 1500
-	msecs_timeout_q = ((msecs_timeout / 2) * random.random()) + msecs_timeout / 2
 	while True:
 		current = time.time()
 		msecs_diff = (current - start) * 1000
 		if msecs_diff > msecs_timeout_q :
-			state = state_table[state]
 			break
 		else: 
 			plot_x = msecs_diff / msecs_timeout_q
 			plot_y = 0
-			if state == 4 :
-				plot_y = Q4(plot_x)
-				pwm_val = pwm_min + ((pwm_mid - pwm_min) * plot_y)
-			elif state == 2 :
-				plot_y = Q2(plot_x)
-				pwm_val = pwm_mid + ((pwm_max - pwm_mid) * plot_y)
-			elif state == 1 :
-				plot_y = Q1(plot_x)
-				pwm_val = pwm_mid + ((pwm_max - pwm_mid) * plot_y)
-			elif state == 3 :
-				plot_y = Q3(plot_x)
-				pwm_val = pwm_min + ((pwm_mid - pwm_min) * plot_y)
-			#print(math.ceil(pwm_val))
-			pi.set_PWM_dutycycle(17, pwm_val)
+			plot_y = QCR(plot_x)
+			val = min + ((max - min) * plot_y)
+			#print(math.ceil(val))
+			pi.set_PWM_dutycycle(17, val)
+
+# unused
+def LPF(min, max, msecs_timeout_q):
+	start = time.time()
+	while True:
+		current = time.time()
+		msecs_diff = (current - start) * 1000
+		if msecs_diff > msecs_timeout_q :
+			break
+		else: 
+			plot_x = msecs_diff / msecs_timeout_q
+			plot_y = 0
+			plot_y = QCF(plot_x)
+			val = min + ((max - min) * plot_y)
+			#print(math.ceil(val))
+			pi.set_PWM_dutycycle(17, val)
+
+# (co)sine loop
+def LP(min, max, msecs_timeout_q):
+	while True:
+		start = time.time()
+		while True:
+			current = time.time()
+			msecs_diff = (current - start) * 1000
+			if msecs_diff > msecs_timeout_q :
+				break
+			else: 
+				plot_x = msecs_diff / msecs_timeout_q
+				plot_y = 0
+				plot_y = QC(plot_x)
+				val = min + ((max - min) * plot_y)
+				#print(math.ceil(val))
+				pi.set_PWM_dutycycle(17, val)
+
+pwm_min = 48
+pwm_max = 255
+
+msecs_timeout = 5000
+
+LPR(0, pwm_min, msecs_timeout * 0.5)
+LP(pwm_min, pwm_max, msecs_timeout)
